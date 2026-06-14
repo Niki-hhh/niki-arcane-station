7fe3b4f24413e1d9338414f042cf8f8d17f9a31d
Vecortys <123871947+Vecortys@users.noreply.github.com>
2026-06-15 01:46:30 +0300

    пара фиксов

--- Content.Server/_Arcane/AutoVoting/AutoVotingSystem.cs
+++ Content.Server/_Arcane/AutoVoting/AutoVotingSystem.cs
@@ -1,15 +1,12 @@
 using Content.Server.Voting.Managers;
 using Content.Shared.GameTicking;
 using Content.Shared.Voting;
-using Robust.Server.Player;
-using System.Linq;
 
 namespace Content.Server._Arcane.AutoVoting;
 
 public sealed partial class AutoVotingSystem : EntitySystem
 {
     [Dependency] private readonly IVoteManager _voteManager = default!;
-    [Dependency] private readonly IPlayerManager _playerManager = default!;
 
     public override void Initialize()
     {
